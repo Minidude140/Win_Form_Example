280b4f05096585ed9de628e851caa54dbef84b21
Zac <zac.minidude140@Gmail.com>
2023-10-09 08:20:10 -0600

    Renamed

--- Win_Form_Example/My Project/Application.Designer.vb
+++ Win_Form_Example/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Win_Form_Example.Form1
+            Me.MainForm = Global.Win_Form_Example.WinFormExampleForm
         End Sub
     End Class
 End Namespace
